2ee69cee3fd64ac65cce05ada5cf17ece1c3c4b5
Yohan Piron <Yinfei84@gmail.com>
2014-09-24 22:40:29 +0200

    Updated client to call production API

--- app/assets/javascripts/controllers/index_controller.js
+++ app/assets/javascripts/controllers/index_controller.js
@@ -1,6 +1,6 @@
 App.controller('indexCtrl', function ($scope, $http, $window, $location){
 
-  $http.get("http://localhost:8888/ETNA/2SN_Core/web/app_dev.php/api/users")
+  $http.get("http://192.168.201.4/web/app_dev.php/api/users")
     .success(function(data){
       $scope.response = data;
     })
