f4311c3464846da9be9e6bb81389fdd99eca19b3
Yohan Piron <Yinfei84@gmail.com>
2014-09-24 20:16:41 +0200

    App now gets Core users !

--- app/assets/javascripts/controllers/index_controller.js
+++ app/assets/javascripts/controllers/index_controller.js
@@ -1,7 +1,8 @@
 App.controller('indexCtrl', function ($scope, $http, $window, $location){
 
-  $http.get("http://172.16.128.168/api/users")
+  $http.get("http://localhost:8888/ETNA/2SN_Core/web/app_dev.php/api/users")
     .success(function(data){
+      $scope.response = data;
     })
     .error(function(data){
      console.log("error =", data);
