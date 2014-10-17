bda3ec1f4dfc30e39022b0f901c8fcb737c847c8
hoverb <hover_b@etna-alternance.net>
2014-10-17 12:24:07 +0200

    set fixtures on account

--- webapp/assets/javascripts/controllers/account_controller.js
+++ webapp/assets/javascripts/controllers/account_controller.js
@@ -1,3 +1,9 @@
 App.controller('accountCtrl', function ($scope, $http, $window, $location){
+  $scope.user = {'username':'Yinfei', 'usermail':'swagster@skyblog.web', 'plan':1, 'avatar':'webapp/assets/img/fixtures-pictures/yinfei.png'};
+
+  $('#account-picture').attr('src', $scope.user.avatar);
+  $('#account-username').html($scope.user.username);
+  $('#account-usermail').val($scope.user.usermail);
+  $('#planSelect').val($scope.user.plan);
 
 });
