2febbe04942166d0e39c44606195d8555e168b7f
Thich-a <antoine.thich@gmail.com>
2014-10-17 19:37:53 +0200

    Mise en place des liaisons angular dans Game Session

--- webapp/assets/javascripts/controllers/game_sessions_show_controller.js
+++ webapp/assets/javascripts/controllers/game_sessions_show_controller.js
@@ -1,95 +1,203 @@
-App.controller('gameSessionsShowCtrl', function ($scope, $http, $window, $location){
+App.controller('gameSessionsShowCtrl', function ($scope, $http, $window, $location, $routeParams){
+
+  $scope.currentGame;
+  $scope.listGameSessions = [];
+  $scope.playerOther = [];
+
   $scope.user = {
                   'id' :       1,
                   'username' : 'Yinfei',
                   'avatar' :   'webapp/assets/img/fixtures-pictures/yinfei.png'
                 }
-  $scope.sheets = [{
-                      'id' : 1,
-                      'char_name' : 'Jill Valentine',
-                      'details'   : 'Level 7 Scout',
-                      'avatar'    : 'webapp/assets/img/fixtures-pictures/jill.jpg',
-                      'game_session' : {
-                                       'id' : 1,
-                                       'name' : 'Arkhlay Manor adventures'
-                                     }
-                  },{
-                      'id' : 2,
-                      'char_name' : 'Big Boss',
-                      'details'   : 'Level 18 Infiltrator',
-                      'avatar'    : 'webapp/assets/img/fixtures-pictures/bb.png',
-                      'game_session' : {
-                                       'id' : 2,
-                                       'name' : 'Operation Snake Eater'
-                                     }
-                  },{
-                      'id' : 3,
-                      'char_name' : 'Super Mario',
-                      'details'   : 'Level 20 Plumber',
-                      'avatar'    : 'webapp/assets/img/fixtures-pictures/mario.gif',
-                      'game_session' : {
-                                       'id' : 3,
-                                       'name' : 'Mushroom Kingdom'
-                                     }
-                  }]
 
   $scope.sessions = [{
                         'id' : 1,
                         'name' : 'Arkhlay Manor adventures',
                         'created_at' : '12 June 1998',
                         'last_log' :   'A zombie opened the door !',
-                        'new_logs' :   true
-                     },{
-                        'id' : 2,
-                        'name' : 'Operation Snake Eater',
-                        'created_at' : '6 August 1956',
-                        'last_log' :   'Critical hit ! Shagohod explodes !',
-                        'new_logs' :   false
-                     },{
-                        'id' : 3,
-                        'name' : 'Mushroom Kingdom',
-                        'created_at' : '2 Febuary 1983',
-                        'last_log' : 'IT\'S A-ME MARIOOOOO !',
-                        'new_logs' :   true
+                        'new_logs' :   true,
+                        'sheet':{
+                                    'id' : 1,
+                                    'char_name' : 'Jill Valentine',
+                                    'details'   : 'Level 7 Scout',
+                                    'avatar'    : 'webapp/assets/img/fixtures-pictures/jill.jpg'
+                                },
+                        'user':[{
+                                'id' :       1,
+                                'username' : 'Yinfei',
+                                'avatar' :   'webapp/assets/img/fixtures-pictures/yinfei.png',
+                                'charaters':{
+                                            'id' : 1,
+                                            'char_name' : 'Jill Valentine',
+                                            'details'   : 'Level 7 Scout',
+                                            'avatar'    : 'webapp/assets/img/fixtures-pictures/jill.jpg',
+                                            'master'    : true
+                                            },
+                                },
+                                {
+                                'id' :       2,
+                                'username' : 'Toinou',
+                                'avatar' :   'webapp/assets/img/fixtures-pictures/chopper.jpg',
+                                'charaters':{
+                                            'id' : 1,
+                                            'char_name' : 'Jin',
+                                            'details'   : 'Level 5 Scout',
+                                            'avatar'    : 'webapp/assets/img/fixtures-pictures/jin.jpg',
+                                            'master'    : false
+                                            },
+                                },
+                                {
+                                'id' :       3,
+                                'username' : 'Lili',
+                                'avatar' :   'webapp/assets/img/fixtures-pictures/chopper.jpg',
+                                'charaters':{
+                                            'id' : 1,
+                                            'char_name' : 'Kitty-Fury',
+                                            'details'   : 'Level 5 Scout',
+                                            'avatar'    : 'webapp/assets/img/fixtures-pictures/hellokitty.jpg',
+                                            'master'    : false
+                                            },
+                                }]
+                       },{
+                          'id' : 2,
+                          'name' : 'Operation Snake Eater',
+                          'created_at' : '6 August 1956',
+                          'last_log' :   'Critical hit ! Shagohod explodes !',
+                          'new_logs' :   false,
+                          'sheet':{
+                                      'id' : 2,
+                                      'char_name' : 'Big Boss',
+                                      'details'   : 'Level 18 Infiltrator',
+                                      'avatar'    : 'webapp/assets/img/fixtures-pictures/bb.png'
+                                  },
+                          'user':[{
+                                  'id' :       1,
+                                  'username' : 'Yinfei',
+                                  'avatar' :   'webapp/assets/img/fixtures-pictures/yinfei.png',
+                                  'charaters':{
+                                              'id' : 2,
+                                              'char_name' : 'Big Boss',
+                                              'details'   : 'Level 18 Infiltrator',
+                                              'avatar'    : 'webapp/assets/img/fixtures-pictures/bb.png',
+                                              'master'    : false
+                                              },
+                                  },
+                                  {
+                                  'id' :       2,
+                                  'username' : 'Toinou',
+                                  'avatar' :   'webapp/assets/img/fixtures-pictures/chopper.jpg',
+                                  'charaters':{
+                                              'id' : 1,
+                                              'char_name' : 'Jin',
+                                              'details'   : 'Level 5 Scout',
+                                              'avatar'    : 'webapp/assets/img/fixtures-pictures/jin.jpg',
+                                              'master'    : true
+                                              },
+                                  },
+                                  {
+                                  'id' :       3,
+                                  'username' : 'Lili',
+                                  'avatar' :   'webapp/assets/img/fixtures-pictures/chopper.jpg',
+                                  'charaters':{
+                                              'id' : 1,
+                                              'char_name' : 'Kitty-Fury',
+                                              'details'   : 'Level 5 Scout',
+                                              'avatar'    : 'webapp/assets/img/fixtures-pictures/hellokitty.jpg',
+                                              'master'    : false
+                                              },
+                                  }],
+                               },{
+                              'id' : 3,
+                              'name' : 'Mushroom Kingdom',
+                              'created_at' : '2 Febuary 1983',
+                              'last_log' : 'IT\'S A-ME MARIOOOOO !',
+                              'new_logs' :   true,
+                              'sheet':{
+                                          'id' : 3,
+                                          'char_name' : 'Super Mario',
+                                          'details'   : 'Level 20 Plumber',
+                                          'avatar'    : 'webapp/assets/img/fixtures-pictures/mario.gif'
+                                      },
+                              'user':[{
+                                      'id' :       1,
+                                      'username' : 'Yinfei',
+                                      'avatar' :   'webapp/assets/img/fixtures-pictures/yinfei.png',
+                                      'charaters':{
+                                                  'id' : 3,
+                                                  'char_name' : 'Super Mario',
+                                                  'details'   : 'Level 20 Plumber',
+                                                  'avatar'    : 'webapp/assets/img/fixtures-pictures/mario.gif',
+                                                  'master'    : false
+                                                  },
+                                      },
+                                      {
+                                      'id' :       2,
+                                      'username' : 'Toinou',
+                                      'avatar' :   'webapp/assets/img/fixtures-pictures/chopper.jpg',
+                                      'charaters':{
+                                                  'id' : 1,
+                                                  'char_name' : 'Jin',
+                                                  'details'   : 'Level 5 Scout',
+                                                  'avatar'    : 'webapp/assets/img/fixtures-pictures/jin.jpg',
+                                                  'master'    : true
+                                                  },
+                                      },
+                                      {
+                                      'id' :       3,
+                                      'username' : 'Lili',
+                                      'avatar' :   'webapp/assets/img/fixtures-pictures/chopper.jpg',
+                                      'charaters':{
+                                                  'id' : 1,
+                                                  'char_name' : 'Kitty-Fury',
+                                                  'details'   : 'Level 5 Scout',
+                                                  'avatar'    : 'webapp/assets/img/fixtures-pictures/hellokitty.jpg',
+                                                  'master'    : false
+                                                  }
+                                      }]
+
                      }]
 
-  $scope.albums = [
-                    {
-                      'id': 1,
-                      'title' : 'my album',
-                      'pictures' :
-                        [{
-                          'id' : 1,
-                          'url' : 'webapp/assets/img/galleries/AmericaBoy.png'
-                          },{
-                            'id' : 2,
-                            'url' : 'webapp/assets/img/galleries/avatar.png'
-                          },{
-                            'id' : 3,
-                            'url' : 'webapp/assets/img/galleries/avengers2SN.png'
-                          },{
-                            'id' : 4,
-                            'url' : 'webapp/assets/img/galleries/cookieMonster.png'
-                          },{
-                            'id' : 5,
-                            'url' : 'webapp/assets/img/galleries/hipsterMax.png'
-                          },{
-                          'id' : 6,
-                          'url' : 'webapp/assets/img/galleries/ninjaTurtles.png'
-                        }]
-                    },{
-                      'id': 2,
-                      'title': 'my other album',
-                      'pictures' :
-                        [{
-                          'id' : 7,
-                          'url' : 'webapp/assets/img/galleries/rubyMan.png'
-                        },{
-                          'id' : 8,
-                          'url' : 'webapp/assets/img/galleries/titanic3D.png'
-                        },{
-                          'id' : 9,
-                          'url' : 'webapp/assets/img/galleries/zionLion.png'
-                        }]
-                  }]
+$scope.selectCurrentGame = function(){
+  for(session in $scope.sessions){
+    if ($scope.sessions[session].id == $routeParams.id)
+    {
+      $scope.currentGame = $scope.sessions[session];
+    }
+  }
+}
+$scope.selectCurrentGame();
+
+$scope.sheet = $scope.currentGame.sheet;
+
+$scope.listGameSessionSelect = function(){
+  var i = 0;
+  for(session in $scope.sessions)
+  {
+    if ($scope.sessions[session].id != $scope.currentGame.id)
+    {
+      $scope.listGameSessions[i] = $scope.sessions[session];
+      i++;
+    }
+  }
+}
+$scope.listGameSessionSelect();
+
+$scope.masterGameSelect = function(){
+  var i = 0;
+  for(game in $scope.currentGame.user)
+  {
+    if ($scope.currentGame.user[game].charaters.master == true)
+    {
+      $scope.playerMaster = $scope.currentGame.user[game];
+    }
+    else
+    {
+      $scope.playerOther[i] = $scope.currentGame.user[game];
+      i++;
+    }
+  }
+}
+$scope.masterGameSelect();
+
+
 });
